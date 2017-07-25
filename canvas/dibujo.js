c3b2ae30141bf366aec80d7f0013d5923eee5cea
Tania Bernal <tanibu18@gmail.com>
2017-07-24 20:57:19 -0500

    Programacion basica Platzi

---
+++ canvas/dibujo.js
@@ -0,0 +1,20 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+
+
+dibujarLinea("purple, 10, 300, 220, 10");
+dibujarLinea("purple, 300, 10, 10, 220");
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+  console.log("mensaje");
+
+}
---
+++ dom/codigo.js
@@ -0,0 +1,4 @@
+var ruta = window.location;
+document.write("Estas en: " + ruta);
+
+alert("Funciona!!!");
