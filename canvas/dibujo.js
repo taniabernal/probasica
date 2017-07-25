47de8d9637971358555a1208bc08388572e9d5dd
Tania Bernal <tanibu18@gmail.com>
2017-07-25 16:53:27 -0500

    Modificaciones en el index se agrega boton y caja de texto con funcion en javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,21 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colour = "#DB2464";
 
-for(l = 0; l < lineas; l++)
- {
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea(colour, 300, yi, xf, 0);
-    dibujarLinea(colour, 0, yi, xf, 300);
-    console.log("lineas" + l);
-
-  }
-  dibujarLinea(colour, 1, 1, 1, 299);
-  dibujarLinea(colour, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -28,4 +17,26 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath();
   console.log("mensaje");
 
+}
+
+
+function dibujoPorClick()
+{
+
+    var lineas = parseInt(texto.value);
+    var l = 0;
+    var yi, xf;
+    var colour = "#DB2464";
+
+for(l = 0; l < lineas; l++)
+     {
+        yi = 10 * l;
+        xf = 10 * (l + 1);
+        dibujarLinea(colour, 300, yi, xf, 0);
+        dibujarLinea(colour, 0, yi, xf, 300);
+        console.log("lineas" + l);
+
+      }
+      dibujarLinea(colour, 1, 1, 1, 299);
+      dibujarLinea(colour, 1, 299, 299, 299);
 }
